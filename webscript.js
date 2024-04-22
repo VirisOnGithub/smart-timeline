1dc568800961288e67af7c16487f7645f0dfd291
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-04-22 16:03:35 +0200

    Added Creator

--- webscript.js
+++ webscript.js
@@ -12,3 +12,27 @@ function addParallel(){
     container.innerHTML += '<div class="parallel" pcolor="'+pcolor+'" pwidth="'+pwidth+'">'+content+'</div>';
     updateParallels();
 }
+
+function updateColor(){
+    let color = document.getElementById('pcolor').value;
+    let button = document.querySelector('.parall-create');
+    button.style.backgroundColor = color;
+}
+
+let color = document.getElementById('pcolor');
+color.addEventListener('input', updateColor);
+
+
+function closeNew () {
+    let newP = document.querySelector('.new');
+    newP.style.visibility = 'hidden';
+    let openBtn = document.querySelector('.open-new');
+    openBtn.style.visibility = 'visible';
+}
+
+function openNew () {
+    let newP = document.querySelector('.new');
+    newP.style.visibility = 'visible';
+    let openBtn = document.querySelector('.open-new');
+    openBtn.style.visibility = 'hidden';
+}
